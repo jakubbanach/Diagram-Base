8d33c1bd74663b153e74995f340aedd9923d62bd
Rys-Nowak <ryszard.nowak012@gmail.com>
2023-05-08 22:27:06 +0200

    sequence classes

--- grammar/diagrams/SequenceDiagram.py
+++ grammar/diagrams/SequenceDiagram.py
@@ -1,5 +1,58 @@
 from .Diagram import Diagram
 
 
+class Lifeline:
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def draw(self):
+        # TODO: Implement this method
+        pass
+
+
+class Message:
+    source: Lifeline
+    target: Lifeline
+    type: str # sync / async / return / create / destroy ...??
+    name: str
+
+    def __init__(self, source: Lifeline, target: Lifeline, type: str, name: str):
+        self.source = source
+        self.target = target
+        self.type = type
+        self.name = name
+
+    def draw(self):
+        # TODO: Implement this method
+        pass
+
+
+# ???
+class Block:
+    type: str # alt / opt / loop / par / ... ??
+    messages: list[Message]
+
+    def __init__(self, type: str):
+        self.type = type
+        self.messages = []
+
+    def draw(self):
+        # TODO: Implement this method
+        pass
+
+
 class SequenceDiagram(Diagram):
-    pass
+    lifelines: list[Lifeline]
+    messages: list[Message]
+    blocks: list[Block]
+
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.actors = []
+        self.messages = []
+
+    def draw(self):
+        # TODO: Implement this method
+        pass
